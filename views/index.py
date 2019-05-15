0d43250776b62f1cb9aaf598d4f428479934ad00
superlv123 <15151824064@163.com>
2019-05-15 16:53:29 +0800

    Initial commit

---
+++ views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+# 本脚由小网工-小布编辑，用于乾颐盾Python课程！
+# 小布QQ：1172090974
+# 码云：https://gitee.com/superlv-tmt/events
+# 一起在网络的道路上协同进步
+
+from django.shortcuts import render_to_response
+
+def index(request):
+    return render_to_response('index.html')
+
+if __name__ == '__main__':
+    pass
